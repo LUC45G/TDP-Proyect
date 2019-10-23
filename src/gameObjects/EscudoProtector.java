6efc4da9fabeb5a4712fb98ddc9db9350417244c
Lucas Gonzalez <lucasgxnzalez@gmail.com>
2019-10-23 20:13:58 -0300

    inicio de clase escudo

---
+++ src/gameObjects/EscudoProtector.java
@@ -0,0 +1,11 @@
+package gameObjects;
+
+public class EscudoProtector extends ImmovableObject {
+
+	public EscudoProtector() {
+		super();
+		// _sprite de escudo protector
+		// hitbox del sprite
+	}
+
+}
--- src/gameObjects/ImmovableObject.java
+++ src/gameObjects/ImmovableObject.java
@@ -1,17 +1,10 @@
 package gameObjects;
 
-import java.awt.Rectangle;
-
-import javax.swing.ImageIcon;
-
 import logicas.Visitor;
 
 public class ImmovableObject extends GameObject {
-
-	public ImmovableObject(ImageIcon sprite, Rectangle hitbox) {
-		super(sprite, hitbox);
-		
-	}
+	
+	protected ImmovableObject() {}
 
 	public void affect(Disparo miDisparo) {
 		//este metodo debe ser redefinido por cada sub_clase de ImmovableObject
